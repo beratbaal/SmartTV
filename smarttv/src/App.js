2869ca507ffcb7849ab15eb679614220a1a08b0b
Berat <64924845+beratbaal@users.noreply.github.com>
2022-05-13 21:50:06 +0300

    Update App.js

--- smarttv/src/App.js
+++ smarttv/src/App.js
@@ -1,13 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import {Sidebar} from './Views/Sidebar';
+import { Channels } from './Views/Channels';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    <Sidebar/>
-    </>
+   <>
+  
+   </>
   );
+
+
+  function MainPage(){
+    return(
+      
+      <Sidebar/>
+      
+      
+    );
+    
+  }
+
+
+  
+
 }
+
+
+
+
 
 export default App;
---
+++ smarttv/src/Views/Channels.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../CSS/Channels.css";
+
+export class Channels extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return(
+            <>
+          
+            </>
+        )
+    }
+}
